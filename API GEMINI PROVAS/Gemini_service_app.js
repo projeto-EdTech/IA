3b0306622cf797aea7aa281c2bf301df760b3ad5
Felipe Grolla Freitas <120500170+Grolla05@users.noreply.github.com>
2025-07-16 03:31:35 -0300

    JSON

--- API GEMINI PROVAS/Gemini_service_app.js
+++ API GEMINI PROVAS/Gemini_service_app.js
@@ -120,11 +120,11 @@ Não crie ou modifique nenhum nome de campo. Respeite os tipos de dados e a estr
   console.log("Iniciando upload e processamento dos arquivos...");
 
     console.time("Processando Arquivo da Prova");
-    let file1 = await uploadRemotePDF("https://www.curso-objetivo.br/vestibular/resolucao-comentada/famerp/2019/1dia/famerp2019_1dia_prova.pdf", "PDF Da Prova");
+    let file1 = await uploadRemotePDF("https://www.curso-objetivo.br/vestibular/resolucao-comentada/unesp/2022/1fase/2dia/UNESP2022_1fase_prova.pdf", "PDF Da Prova");
     console.timeEnd("Processando Arquivo da Prova"); // Termina o cronômetro do upload da prova
 
     console.time("Processando Arquivo do Gabarito");
-    let file2 = await uploadRemotePDF("https://www.curso-objetivo.br/vestibular/resolucao-comentada/famerp/2019/1dia/famerp2019_1dia_gabarito.pdf", "PDF Do Gabarito");
+    let file2 = await uploadRemotePDF("https://www.curso-objetivo.br/vestibular/resolucao-comentada/unesp/2022/1fase/2dia/UNESP2022_1fase_gabarito.pdf", "PDF Do Gabarito");
     console.timeEnd("Processando Arquivo do Gabarito"); // Termina o cronômetro do upload do Gabarito
 
     console.log("Uploads e processamento dos PDF's concluídos.");
